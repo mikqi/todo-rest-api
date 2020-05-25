06cd9a8af05c625bedf61617a50cdaf2bf30907d
Muhammad Rivki <Muhammad.Rivki@live.com>
2020-05-25 17:01:16 +0700

    :sparkles: add logger and prettier

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+const logger = require('morgan')
 const cors = require('cors')
 const app = express()
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb://localhost:27017/learn-api', {
   useUnifiedTopology: true,
 })
 
+app.use(logger('dev'))
 app.use(
   bodyParser.urlencoded({
     extended: true,
